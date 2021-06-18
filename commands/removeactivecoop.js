d1121689d495164667670ab72acd4bea1019679b
Rainer Wittmann <r-wittmann@outlook.de>
2021-06-18 20:06:14 +0200

    remove unused warning

--- commands/removeactivecoop.js
+++ commands/removeactivecoop.js
@@ -2,21 +2,13 @@ const {moveActiveCoopChannelToArchive} = require("../controllers/coopChannels.js
 const {removeActiveCoop} = require("../controllers/activeCoop.js");
 const {getActiveCoops} = require("../controllers/activeCoop.js");
 
-const {getWrongChannelAskModMessage} = require("../messageGenerators/wrongChannelAskModMessage.js");
-
 module.exports = {
     name: "removeactivecoop",
     usage: "<contract-id> <coop-name>",
-    description: "Removes a coop to the list of active coops. This command is only available for the mods in a special channel.",
+    description: "Removes a coop to the list of active coops.",
     async execute(message, args) {
         const contractId = args[0];
         const coopCode = args[1];
-
-        // this command should only be available for the mods in a specific channel
-        // this is checked here, before anything else is done
-        if (message.channel.id !== process.env.BOT_CHANNEL_ID) {
-            message.channel.send(getWrongChannelAskModMessage());
-        }
 
         // extract active coops from the "active coops" channel
         const activeCoops = await getActiveCoops(message.client);
