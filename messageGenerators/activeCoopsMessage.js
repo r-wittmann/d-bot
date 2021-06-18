db0f19d1a4343760416c32f229cc7c81bbfa75eb
Rainer Wittmann <r-wittmann@outlook.de>
2021-06-18 20:24:07 +0200

    add coop tracker link to active coop message

--- messageGenerators/activeCoopsMessage.js
+++ messageGenerators/activeCoopsMessage.js
@@ -1,10 +1,10 @@
 
-const generateCoopFields = (coops, maxCoopSize, curCoopSizes) => {
+const generateCoopFields = (contractId, coops, maxCoopSize, curCoopSizes) => {
     let coopObject = [];
     coops.forEach((coop, i) => {
         coopObject.push({
             name: `Coop Code "${coop}"`,
-            value: `${curCoopSizes ? curCoopSizes[i] : "xx"}/${maxCoopSize} players`,
+            value: `${curCoopSizes ? curCoopSizes[i] : "xx"}/${maxCoopSize} players\n[Coop Tracker](https://eicoop.netlify.app/${contractId}/${coop})`,
         })
     })
     return coopObject;
@@ -17,7 +17,7 @@ exports.getActiveCoopsMessage = (contractName, contractId, coopCodes, maxCoopSiz
         title: contractName,
         description: `Here is a list of all currently active coops for ${contractName} (${contractId})`,
         fields: [
-            generateCoopFields(coopCodes, maxCoopSize, curCoopSizes)
+            generateCoopFields(contractId, coopCodes, maxCoopSize, curCoopSizes)
         ],
         footer: {
             text: `Max coop size: ${maxCoopSize}`
