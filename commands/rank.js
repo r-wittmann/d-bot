d6775b05b98dffd47abd9529b09df247da405b34
Rainer Wittmann <r-wittmann@outlook.de>
2021-07-18 09:45:21 +0200

    implement rank command

---
+++ commands/rank.js
@@ -0,0 +1,23 @@
+const {generateRanking} = require("../controllers/rank.js");
+
+module.exports = {
+    name: "rank",
+    usage: "<type>",
+    description: "Returns the current ranking of members on various options. <type> can be one of 'EB' (earnings bonus), 'SE' (soul eggs), 'PE' (eggs of prophecy), 'GE' (golden eggs current), 'GET' (golden eggs total) or 'D' (drones).",
+    async execute(message, args) {
+        const type = args[0];
+        let waitingMessage;
+
+        try {
+            // ignore warning below. That's not true
+            waitingMessage = await message.channel.send("This may take a few moments\nWorking on it...");
+
+            await generateRanking(message, type);
+        } catch (e) {
+            message.channel.send("Something went wrong\n" + e.message);
+        }
+
+        // remove "in progress" message
+        await waitingMessage.delete();
+    },
+};
