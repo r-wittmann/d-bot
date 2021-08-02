e43b4e9803deaf0270d4d17b84f403b9b541a68e
Rainer Wittmann <r-wittmann@outlook.de>
2021-08-02 09:51:54 +0200

    implement addmember command

---
+++ commands/members/addmember.js
@@ -0,0 +1,23 @@
+const {addMember} = require("../../controllers/members/members.js");
+const {log} = require("../../services/logService.js");
+
+module.exports = {
+    name: "addmember",
+    usage: "<EI-id> <in-game-name> <@discord-user>",
+    description: "Adds a new member to the database. If the discord id is not provided, the bot assumes the author of the message to be the new player.",
+    help: "Adds a new member to the database of players. Please provide the EI-id (EI + 16 numbers), the in game name and mention the new user (@UserName). Instead of the user mention, you can also provide the discord id (18 numbers).\n" +
+        "If the discord user or id is not provided, the bot assumes the author of the message to be the new player.",
+    async execute(message, args) {
+        const eiId = args[0];
+        const inGameName = args[1];
+        const discordId = args[2];
+
+        try {
+
+            await addMember(message, eiId, inGameName, discordId);
+        } catch (e) {
+            await log(message.client, e.message, e.stack);
+            await message.channel.send("Something went wrong.\n" + e.message);
+        }
+    },
+};
--- controllers/members/members.js
+++ controllers/members/members.js
@@ -1,7 +1,29 @@
 const {log} = require("../../services/logService.js");
 const {getMemberListMessage} = require("../../messageGenerators/membersListMessage.js");
 const {getDiscordName} = require("../../services/dataAccess/discord.js");
-const {getMembers} = require("../../services/dataAccess/database.js");
+const {addMember, getMembers} = require("../../services/dataAccess/database.js");
+
+exports.addMember = async (message, eiId, inGameName, discordId) => {
+    // check the ei id for a match to the general pattern
+    if (!/^(EI)([0-9]{16})/.test(eiId)) {
+        await message.channel.send(`The provided EI-id seems to be wrong. It is generally formatted as EI + 16 numbers. You provided \`${eiId}\``);
+        await log(message.client, `Add member failed because of a faulty EI-id: \`${eiId}\``);
+        return;
+    }
+
+    discordId = discordId || message.author.id;
+    // to allow mentions as argument for the discord user, some characters have to be removed
+    discordId = discordId.replace(/[\\<>@#&!]/g, "");
+
+    try {
+        await addMember(eiId, discordId, inGameName);
+        await log(message.client, "A member was added to the database", eiId, inGameName, discordId);
+        await message.channel.send("Member added");
+    } catch (e) {
+        throw e;
+    }
+
+}
 
 exports.getMembers = async (message) => {
     try {
